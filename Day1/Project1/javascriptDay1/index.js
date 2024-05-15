eeb9ad55229c84e7fadb71b70cbfadcce6650096
Pradip Aadhikari <pradipaadhikari@Pradips-MacBook-Air.local>
2024-05-15 11:31:37 +0545

    i am writing  javascript

---
+++ Day1/Project1/javascriptDay1/index.js
@@ -0,0 +1,47 @@
+
+
+const students=["mamta","suraj","anurag"];
+
+console.log(students);
+const popanurag=students.pop();
+console.log(popanurag);
+console.log(students);
+
+const popsuraj=students.pop();
+console.log(popsuraj);
+console.log(students);
+
+const popmamta=students.pop();
+console.log(popmamta);
+console.log(students);
+
+
+
+const hi =students.push("mamta")
+console.log(students);
+
+const hello =students.push("suraj")
+console.log(students);
+
+const hel =students.push("anurag")
+console.log(students);
+
+students.unshift("sagar");
+console.log(students);
+students.shift("sagar");
+console.log(students);
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+  const rplce =fruits.copyWithin(2, 0);
+  console.log(rplce);
+
+
+
+
+
+
+
+
+
+    
